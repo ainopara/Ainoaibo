5d4d50d98a0563fe7752c00f762bfa87ccee7923
ainopara <ainopara@gmail.com>
2018-06-13 23:16:09 +0800

    StateTransitionHehavior now have a default initializer and can handle Action.

--- Ainoaibo/StateTransition/StateTransition.swift
+++ Ainoaibo/StateTransition/StateTransition.swift
@@ -1,13 +1,20 @@
-open class StateTransitionBehavior<State> {
-    public enum TransitionRuleResult {
-        case allow
-        case reject
-        case notApplicable
+
+public enum StateTransitionRuleResult {
+    case allow
+    case reject
+    case notApplicable
+}
+
+open class StateTransitionBehavior<ViewModel: AnyObject, State, Action> {
+    public weak var viewModel: ViewModel?
+
+    public init(viewModel: ViewModel) {
+        self.viewModel = viewModel
     }
 
     public init() {}
 
-    open func checkTransition(from: State, to: State) -> TransitionRuleResult { return .notApplicable }
-    open func preTransition(from: State, to: State) {}
-    open func postTransition(from: State, to: State) {}
+    open func checkTransition(action: Action, from: State, to: State) -> StateTransitionRuleResult { return .notApplicable }
+    open func preTransition(action: Action, from: State, to: State) {}
+    open func postTransition(action: Action, from: State, to: State) {}
 }
